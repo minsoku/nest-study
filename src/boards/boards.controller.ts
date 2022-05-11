887e07717e008f9bd96732898572ff538d06a36d
민석 / MIN / ミンソク <minsoku03@gmail.com>
2022-05-11 23:22:16 +0900

    게시물 생성 기능 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
   @Get('/')
-  getAllBoard() {
+  getAllBoard(): Board[] {
     return this.boardService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.createBoard(title, description);
   }
 }
